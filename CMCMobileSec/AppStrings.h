f2f4125d3fd8cbac624b59d250c6e0523a1c259a
Duc Tran <ductran@Duc-Trans-MacBook-Pro.local>
2013-01-02 00:38:24 +0700

    add new file AppStrings.h

---
+++ CMCMobileSec/AppStrings.h
@@ -0,0 +1,14 @@
+//
+//  AppStrings.h
+//  CMCMobileSec
+//
+//  Created by Duc Tran on 1/2/13.
+//  Copyright (c) 2013 CMC. All rights reserved.
+//
+
+#ifndef CMCMobileSec_AppStrings_h
+#define CMCMobileSec_AppStrings_h
+
+
+
+#endif
