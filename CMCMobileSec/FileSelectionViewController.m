694ea162f5ca412549f41c1b269ee6f72c10c975
Duc Tran <ductran@Duc-Trans-MacBook-Pro.local>
2013-01-08 00:12:42 +0700

    xoa tab scan, dua tab summary len dau tien

--- CMCMobileSec/FileSelectionViewController.m
+++ CMCMobileSec/FileSelectionViewController.m
@@ -201,7 +201,8 @@ BOOL isLoadByUpButton = false;
     
     // add button for checkmark
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
-    CGRect frame = CGRectMake(267.0, 3.0, 38.0, 38.0);
+//    CGRect frame = CGRectMake(267.0, 3.0, 38.0, 38.0);
+     CGRect frame = CGRectMake(267.0, 3.0, theImage.size.width, theImage.size.height);
     button.frame = frame;
     [button setBackgroundImage:theImage forState:UIControlStateNormal];
     [button addTarget:self action:@selector(checkButtonTapped:event:)  forControlEvents:UIControlEventTouchUpInside];
